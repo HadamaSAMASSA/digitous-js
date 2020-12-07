b3efdbb6f539c7e08d27680b477e6c78cced8bcb
BlasDams <samassaadama@hotmail.fr>
2020-12-07 17:50:57 +0100

    exo jour01

---
+++ jour1/jour01.js
@@ -0,0 +1,28 @@
+//exercice 01
+console.log("Hello world !");
+
+//exercice 02
+var test = "My name is Adama SAMASSA";
+console.log(test);
+
+//exercice 03
+var name = "Adama SAMASSA";
+console.log("Nice to meet you "+name);
+
+//exercice 04
+var testLength = "I'm very long !";
+console.log(testLength.length);
+
+//exercice 05
+var food = "croissant is meh";
+console.log(food.replace("meh", "so good"));
+
+//exrecice 06
+var basic = "This is cool";
+var basicUp = basic.toUpperCase();
+var basicDown = basic.toLowerCase();
+console.log(basic, basicUp, basicDown);
+
+//exercice 07
+var word = "banana";
+var letters = word.split()
