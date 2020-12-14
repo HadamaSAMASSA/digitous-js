51961dbf092872355ae07da053565bc73deec22c
BlasDams <samassaadama@hotmail.fr>
2020-12-14 17:35:46 +0100

    ex03 even

--- jour04.js
+++ jour04.js
@@ -21,3 +21,51 @@ var cat2= {
     isCute : false
 }
 console.log(cat.age, cat2.isCute);
+//fin ex2
+
+console.log("________03 Even________");
+//ex3
+function checkIfEven(){
+    var num = 2
+    if(num % 2 === 0){
+        console.log("even");
+    }
+    else{
+        console.log("odd");
+    }
+}
+checkIfEven();
+////////////////////////
+function checkIfEven2(){
+    var num = 3;
+    if(num % 2 === 0){
+        console.log("even");
+    }
+    else{
+        console.log("odd");
+    }
+}
+checkIfEven2();
+////////////////////////
+function checkIfEven3(){
+    var num = 321;
+    if(num % 2 === 0){
+        console.log("even");
+    }
+    else{
+        console.log("odd");
+    }
+}
+checkIfEven3();
+////////////////////////
+    var num = 556;
+function checkIfEven4(num){
+
+    if(num % 2 === 0){
+        console.log("even");
+    }
+    else{
+        console.log("odd");
+    }
+}
+checkIfEven4(537);
