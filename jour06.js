21d339fbeb060ebbcf1b4bbaf171e153e76be4a1
BlasDams <samassaadama@hotmail.fr>
2020-12-17 14:40:27 +0100

    ex01 Alphabet

--- jour06.js
+++ jour06.js
@@ -1,8 +1,24 @@
 console.log("________01 Format________");
 //ex1
-function formatDate(){
-    var today = new Date();
-    console.log(today);
+function formatDate(string){
+    var today = new Date(string);
+    var day = today.getDate();
+    var month = today.getMonth()+1;
+    var year = today.getFullYear();
+    console.log(`${day}/${month}/${year}`);
 }
-formatDate()
+formatDate("2020-12-16");
+
+function formatDate2(string){
+    var today = new Date(string)
+    console.log(`${today.getDate()}/${today.getMonth()+1}/${today.getFullYear()}`)
+}
+formatDate2("2020-12-16");
 //fin ex1
+
+console.log("________02 Age________");
+//ex 2
+function calculateAge(){
+    var birthDay = new Date(dateString); 
+}
+calculateAge("03/12/2010");
---
+++ jour07.js
@@ -0,0 +1,11 @@
+console.log("________01 Alphabet________");
+//ex1
+function sortLetters(stringLetters){
+var letters = stringLetters.sort().join();
+console.log(letters);
+}
+sortLetters(["k","o","n","e","x","i","o"]);
+//fin ex1
+
+console.log("________02 XOXO________");
+//ex2
