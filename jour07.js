83d19260cb0ec63ec2b539763b8acd3a703d267b
BlasDams <samassaadama@hotmail.fr>
2020-12-18 11:48:27 +0100

    ex2 et 3 + petite partie bonus 3

--- jour07.js
+++ jour07.js
@@ -9,3 +9,44 @@ sortLetters("konexio");
 
 console.log("________02 XOXO________");
 //ex2
+function countEach(stringCount){
+    var error = "Error : Il n'y a ni \"x\" ni \"o\" !"
+    var xCount = 0;
+    var oCount = 0;
+    for(var i = 0; i < stringCount.length; i++){
+        if(stringCount[i] === "x"){
+            xCount++
+        }
+        else if(stringCount[i] === "o"){
+            oCount++;
+        }
+    }
+
+    if(xCount === 0 && oCount === 0){
+            return error;
+        }
+    else if(xCount === oCount){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+console.log(countEach("Adama"));
+//fin ex2
+
+console.log("________03 Palindrome________");
+//ex3
+function checkPal(stringCheck){
+    var pal = stringCheck.split("").reverse().join("");
+    var isAPal = "Palindrome !"
+    var notAPal = "Nope"
+    if(stringCheck === pal){
+        return isAPal;
+    }
+    else{
+        return notAPal;
+    }
+}
+console.log(checkPal("assa"));
+//fin ex3
